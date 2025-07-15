b1337a33e700bd8f518a4fb47aabcad0f02aabbc
Dimitris Pallas <mc19054@mail.ntua.gr>
2025-07-15 20:44:47 +0300

    Updated example

--- examples/laplace/main.cpp
+++ examples/laplace/main.cpp
@@ -11,34 +11,36 @@ int main(int argc, char **argv)
     auto mesh = io::read_mesh(argv[1]);
     int dim = mesh->topology()->dim();
 
-    int order = 1;
-    auto phi = std::make_shared<fem::CGSpace>(mesh, order, std::vector<std::string>{"T"});
+    int order = 2;
+    auto V = std::make_shared<fem::CGSpace>(mesh, order);
+    auto T = std::make_shared<fem::FEFunction>(V, std::vector<std::string>{"T"});
 
     // Diffusion coefficient
-    auto coeff = std::make_shared<ConstantFunction>(1.0);
+    auto coeff = std::make_shared<fem::ConstantCoefficient>(1.0);
 
     // LHS matrix
-    auto A = fem::petsc::create_mat(*phi);
-    fem::assemble_matrix_cells(*phi, "Solid", fem::kernels::Diffusion3D(coeff), fem::petsc::create_matset(A));
+    auto A = fem::petsc::create_mat(*V, 1);
+    fem::assemble_matrix_cells(*V, "Solid", 1, fem::kernels::Diffusion3D(coeff), fem::petsc::create_matset(A));
     A.assemble();
 
     // RHS vector
-    auto b = fem::petsc::create_vec(*phi);
+    auto b = fem::petsc::create_vec(*V, 1);
     b.assemble();
 
     // Dirichlet B.C.
-    fem::DirichletBC bc(phi);
-    bc.set_value("Left", "T", 10);
-    bc.set_value("Right", "T", 100);
+    fem::DirichletBC bc(T);
+    bc.set_value("Fixed", "T", 10);
+    bc.set_value("Free", "T", 100);
 
     // Solution vector
-    auto x = fem::petsc::create_vec(*phi);
+    auto x = la::petsc::create_vec(*T);
 
     // Apply Dirichlet BC and solve
     fem::petsc::solve(A, b, x, bc);
+    T->assemble();
 
     // Write solution to file
-    io::vtk::write("post/solution_000", *phi, x.get_values());
+    io::vtk::write("post/solution_000", {T});
 
     return 0;
 }
