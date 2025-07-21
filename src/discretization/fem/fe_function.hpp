245d4ae4267b0f3ec2aae1798f7912b8d6c46c82
Dimitris Pallas <mc19054@mail.ntua.gr>
2025-07-21 16:13:48 +0300

    Added doc.

--- src/discretization/fem/fe_function.hpp
+++ src/discretization/fem/fe_function.hpp
@@ -5,17 +5,24 @@
 
 namespace sfem::fem
 {
+    /// @brief Function defined on a finite element space
     class FEFunction : public Function
     {
     public:
+        /// @brief Create a FEFunction for a given space
+        /// @param fe_space Finite element space
+        /// @param components Component names
         FEFunction(std::shared_ptr<const FESpace> fe_space,
                    const std::vector<std::string> &components);
 
+        /// @brief Get the finite element space
         std::shared_ptr<const FESpace> space() const;
 
+        /// @brief Get the values for a given cell
         la::DenseMatrix cell_values(int cell_idx) const;
 
     private:
+        /// @brief Finite element space
         std::shared_ptr<const FESpace> fe_space_;
     };
 }
--- src/discretization/fvm/fv_space.hpp
+++ src/discretization/fvm/fv_space.hpp
@@ -4,34 +4,55 @@
 
 namespace sfem::fvm
 {
-    /// @brief
+    /// @brief A FVSpace stores and readily provides
+    /// geometric information often used in finite volume
+    /// discretizations, e.g. facet area and normal vector,
+    /// along with DoF information, similar to FESpace
+    /// for finite element problems
     class FVSpace
     {
     public:
+        /// @brief Create a FVSpace for a given mesh
+        /// @param mesh The mesh
         FVSpace(std::shared_ptr<const mesh::Mesh> mesh);
 
+        /// @brief Get the mesh
         std::shared_ptr<const mesh::Mesh> mesh() const;
 
+        /// @brief Get the DoF-to-DoF connectivity
         std::shared_ptr<const graph::Connectivity> connectivity() const;
 
+        /// @brief Get the DoF index map
         std::shared_ptr<const IndexMap> index_map() const;
 
+        /// @brief Get the volume for a given cell
         real_t cell_volume(int cell_idx) const;
 
+        /// @brief Get the area for a given facet
         real_t facet_area(int facet_idx) const;
 
+        /// @brief Get the midpoint for a given cell
         std::array<real_t, 3> cell_midpoint(int cell_idx) const;
 
+        /// @brief Get the midpoint for a given facet
         std::array<real_t, 3> facet_midpoint(int facet_idx) const;
 
+        /// @brief Get the normal vector for a given facet
+        /// @note Normalized
         geo::Vec3 facet_normal(int facet_idx) const;
 
+        /// @brief Get the distance between the midpoints of the
+        /// two adjacent cells at a given facet
         geo::Vec3 intercell_distance(int facet_idx) const;
 
+        /// @brief Get the distances between the adjacent cell midpoints
+        /// and the facet midpoint, for a given facet
         std::array<real_t, 2> facet_cell_distances(int facet_idx) const;
 
+        /// @brief Get the indices of the two adjacent cells at a facet
         std::array<int, 2> facet_adjacent_cells(int facet_idx) const;
 
+        /// @brief Check whether a given facet is located on the boundary
         bool is_boundary(int facet_idx) const;
 
     private:
