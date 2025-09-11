d8270bc0b7404ea26f1b9e7443a44992a214605b
Dimitris Pallas <mc19054@mail.ntua.gr>
2025-09-12 02:23:11 +0300

    Added doc. + removed gradient()

--- src/discretization/fvm/fv_function.hpp
+++ src/discretization/fvm/fv_function.hpp
@@ -5,18 +5,21 @@
 
 namespace sfem::fvm
 {
+    /// @brief Function defined on a finite volume space
     class FVFunction : public Function
     {
     public:
+        /// @brief Create a FVFunction
+        /// @param fv_space Finite volume space
+        /// @param components Component names
         FVFunction(std::shared_ptr<const FVSpace> fv_space,
                    const std::vector<std::string> &components);
 
+        /// @brief Get the finite volume space
         std::shared_ptr<const FVSpace> space() const;
 
     private:
+        /// @brief Finite volume space
         std::shared_ptr<const FVSpace> fv_space_;
     };
-
-    void gradient(const FVFunction &phi, FVFunction &grad);
-    std::shared_ptr<FVFunction> gradient(const FVFunction &phi);
 }
