17b84a277b1e9352c0748e3da2bb1173a90dbd62
Dimitris Pallas <mc19054@mail.ntua.gr>
2025-09-22 06:04:44 +0300

    Minor change

--- src/discretization/fvm/utils/la_utils.hpp
+++ src/discretization/fvm/utils/la_utils.hpp
@@ -5,10 +5,10 @@
 
 namespace sfem::fvm
 {
-    /// @brief Create a PETSc vector for a finite volume function
+    /// @brief Create a vector for a finite volume function
     la::Vector create_vec(const FVFunction &phi);
 
-    /// @brief Create a PETSc matrix for a finite volume function
+    /// @brief Create a matrix for a finite volume function
     la::SparseMatrix create_mat(const FVFunction &phi);
 }
 
