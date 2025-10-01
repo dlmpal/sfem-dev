dfe025b6e3c8382d2b36e6b029ed969192077569
Dimitris Pallas <mc19054@mail.ntua.gr>
2025-10-01 13:06:30 +0300

    Use native la solver for example

--- examples/fvm/laplace/main.cpp
+++ examples/fvm/laplace/main.cpp
@@ -1,5 +1,6 @@
 #include "sfem.hpp"
 #include <iostream>
+#include <numeric>
 
 using namespace sfem;
 
@@ -7,34 +8,47 @@ int main(int argc, char **argv)
 {
     initialize(argc, argv);
 
+    // Read the mesh from file
     auto mesh = io::read_mesh(argv[1], mesh::PartitionCriterion::shared_facet);
-    auto topology = mesh->topology();
 
+    // Finite volume space and functions
     auto V = std::make_shared<fvm::FVSpace>(mesh);
     auto T = std::make_shared<fvm::FVFunction>(V, std::vector<std::string>{"T"});
+    auto gradT = std::make_shared<fvm::FVFunction>(V, std::vector<std::string>{"Tx", "Ty"});
 
     // LHS matrix
-    auto A = fvm::petsc::create_mat(*T);
+    auto A = fvm::create_mat(*T);
 
     // RHS vector
-    auto b = fvm::petsc::create_vec(*T);
+    auto b = fvm::create_vec(*T);
 
+    // Setup Dirichlet B.C.
+    fvm::FVBC bc(T);
+    bc.set_value("Left", "T", fvm::BCType::dirichlet, 10);
+    bc.set_value("Right", "T", fvm::BCType::dirichlet, 100);
+
+    // Assemble the LHS matrix and RHS vector
     ConstantCoefficient coeff(1.0);
-
-    fvm::isotropic_diffusion(*T, coeff, "Solid", la::petsc::create_matset(A), la::petsc::create_vecset(b));
-    fvm::dirichlet_bc(*T, coeff, "Left", 10, la::petsc::create_matset(A), la::petsc::create_vecset(b));
-    fvm::dirichlet_bc(*T, coeff, "Right", 100, la::petsc::create_matset(A), la::petsc::create_vecset(b));
-
+    fvm::diffusion(*T, *gradT, bc, coeff,
+                   la::create_matset(A),
+                   la::create_vecset(b));
     A.assemble();
     b.assemble();
+    b.update_ghosts();
 
-    auto x = la::petsc::create_vec(*T);
-    x.assemble();
+    // Solve the resulting linear system using GMRES
+    const real_t tol = 1e-5;
+    const int n_iter_max = 600;
+    const bool verbose = true;
+    const int n_restart = 300;
+    la::GMRES solver(tol, n_iter_max, verbose, n_restart);
+    solver.run(A, b, *T);
 
-    la::petsc::solve(A, b, x);
-    T->assemble();
+    // Update ghosted cell values (required for plotting)
+    T->update_ghosts();
 
-    io::vtk::write("post/solution_000", *mesh, {T});
+    // Save solution to file
+    io::vtk::write(std::format("post/solution_000"), *mesh, {T});
 
     return 0;
 }
