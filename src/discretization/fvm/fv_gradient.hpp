34a42d16f879b5773b6c78d660553103b322ea97
Dimitris Pallas <mc19054@mail.ntua.gr>
2025-10-17 21:49:24 +0300

    Added doc.

--- src/discretization/fvm/fv_gradient.hpp
+++ src/discretization/fvm/fv_gradient.hpp
@@ -4,20 +4,30 @@
 
 namespace sfem::fvm
 {
+    /// @brief Available gradient evaluation methods
     enum GradientMethod
     {
-        green_gauss = 0
+        green_gauss,
+        least_squares
     };
 
-    ///
+    /// @brief Compute the gradient of a finite-volume function
+    /// @param phi Finite-volume function
+    /// @param bc Boundary condition
+    /// @param grad Function gradient
+    /// @param method Gradient evaluation method
     void gradient(const FVFunction &phi,
                   const FVBC &bc,
                   FVFunction &grad,
                   GradientMethod method = GradientMethod::green_gauss);
 
-    ///
-    std::shared_ptr<FVFunction> gradient(const FVFunction &phi,
-                                         const FVBC &bc,
-                                         GradientMethod method = GradientMethod::green_gauss);
+    /// @brief Compute the gradient of a finite-volume function
+    /// @param phi Finite-volume function
+    /// @param bc Boundary condition
+    /// @param method Gradient evaluation method
+    /// @return Function gradient
+    [[nodiscard]] std::shared_ptr<FVFunction> gradient(const FVFunction &phi,
+                                                       const FVBC &bc,
+                                                       GradientMethod method = GradientMethod::green_gauss);
 
 }
