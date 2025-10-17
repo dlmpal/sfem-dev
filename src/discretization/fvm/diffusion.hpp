23613deeb4192e546745942244e9b73f9972cc51
Dimitris Pallas <mc19054@mail.ntua.gr>
2025-10-17 21:49:06 +0300

    Added doc.

--- src/discretization/fvm/diffusion.hpp
+++ src/discretization/fvm/diffusion.hpp
@@ -6,6 +6,14 @@
 
 namespace sfem::fvm
 {
+    /// @brief Assemble the LHS matrix and RHS vector for a diffusion operator
+    /// acting on a scalar finite-volume function
+    /// @param phi Finite-volume function
+    /// @param grad Function gradient
+    /// @param bc Boundary condition
+    /// @param coeff Diffusion coefficient
+    /// @param lhs LHS matrix
+    /// @param rhs RHS vector
     void diffusion(const FVFunction &phi,
                    const FVFunction &grad,
                    const FVBC &bc,
