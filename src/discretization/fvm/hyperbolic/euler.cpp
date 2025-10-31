a4535ff77861841cadd91f5e09bedcee2440e5e6
Dimitris Pallas <mc19054@mail.ntua.gr>
2025-10-31 07:11:23 +0200

    Generalized Euler flux function to both 2D and 3D

--- src/discretization/fvm/hyperbolic/euler.cpp
+++ src/discretization/fvm/hyperbolic/euler.cpp
@@ -4,78 +4,75 @@
 namespace sfem::fvm
 {
     //=============================================================================
-    Euler2D::Euler2D(real_t gamma)
-        : FluxFunction(4, 2), gamma_(gamma)
+    EulerFlux::EulerFlux(real_t gamma, int dim)
+        : FluxFunction(dim + 2, dim),
+          gamma_(gamma)
     {
     }
     //=============================================================================
-    std::array<real_t, 4> Euler2D::compute_flux_x(real_t rho, real_t u, real_t v, real_t E, real_t p) const
+    real_t EulerFlux::compute_flux(const std::vector<real_t> &state,
+                                   std::vector<real_t> &flux, int dir) const
     {
-        return {rho * u,
-                rho * u * u + p,
-                rho * u * v,
-                u * (E + p)};
-    }
-    //=============================================================================
-    std::array<real_t, 4> Euler2D::compute_flux_y(real_t rho, real_t u, real_t v, real_t E, real_t p) const
-    {
-        return {rho * v,
-                rho * u * v,
-                rho * v * v + p,
-                v * (E + p)};
-    }
-    //=============================================================================
-    real_t Euler2D::compute_flux(const std::vector<real_t> &state, std::vector<real_t> &flux, int dir) const
-    {
+        // Get state variables
         const real_t rho = state[0];
-        const real_t u = state[1] / rho;
-        const real_t v = state[2] / rho;
-        const real_t E = state[3];
-        const real_t KE = 0.5 * rho * (u * u + v * v);
-        const real_t p = (gamma_ - 1.0) * (E - KE);
-
-        std::array<real_t, 4> f;
-        if (dir == 0)
+        geo::Vec3 v;
+        for (int i = 0; i < dim_; i++)
         {
-            f = compute_flux_x(rho, u, v, E, p);
+            v(i) = state[i + 1] / rho;
         }
-        else if (dir == 1)
+        const real_t v_mag = v.mag();
+        const real_t KE = 0.5 * rho * v_mag * v_mag;
+        const real_t E = state[dim_ + 1];
+        const real_t e = (E - KE) / rho;
+        const real_t p = (gamma_ - 1) * rho * e;
+
+        // Compute fluxes
+        flux[0] = rho * v(dir);
+        for (int i = 0; i < dim_; i++)
         {
-            f = compute_flux_y(rho, u, v, E, p);
+            flux[i + 1] = rho * v(i) * v(dir);
         }
+        flux[dir + 1] += p;
+        flux[dim_ + 1] = (E + p) * v(dir);
 
-        flux[0] = f[0];
-        flux[1] = f[1];
-        flux[2] = f[2];
-        flux[3] = f[3];
-
-        const real_t sound = std::sqrt(gamma_ * p / rho);
-        const real_t speed = std::sqrt(2.0 * KE / rho);
+        // Max. wavespeed
+        const double speed = v_mag;
+        const double sound = std::sqrt(gamma_ * p / rho);
 
         return sound + speed;
     }
     //=============================================================================
-    real_t Euler2D::compute_normal_flux(const std::vector<real_t> &state,
-                                        const geo::Vec3 &n,
-                                        std::vector<real_t> &normal_flux) const
+    real_t EulerFlux::compute_normal_flux(const std::vector<real_t> &state,
+                                          const geo::Vec3 &normal,
+                                          std::vector<real_t> &normal_flux) const
     {
+        // Get state variables
         const real_t rho = state[0];
-        const real_t u = state[1] / rho;
-        const real_t v = state[2] / rho;
-        const real_t E = state[3];
-        const real_t KE = 0.5 * rho * (u * u + v * v);
-        const real_t p = (gamma_ - 1.0) * (E - KE);
+        geo::Vec3 v;
+        for (int i = 0; i < dim_; i++)
+        {
+            v(i) = state[i + 1] / rho;
+        }
+        const real_t v_mag = v.mag();
+        const real_t KE = 0.5 * rho * v_mag * v_mag;
+        const real_t E = state[dim_ + 1];
+        const real_t e = (E - KE) / rho;
+        const real_t p = (gamma_ - 1) * rho * e;
 
-        const auto f_x = compute_flux_x(rho, u, v, E, p);
-        const auto f_y = compute_flux_y(rho, u, v, E, p);
+        // Compute normal speed
+        const real_t v_normal = geo::inner(v, normal);
 
-        normal_flux[0] = f_x[0] * n.x() + f_y[0] * n.y();
-        normal_flux[1] = f_x[1] * n.x() + f_y[1] * n.y();
-        normal_flux[2] = f_x[2] * n.x() + f_y[2] * n.y();
-        normal_flux[3] = f_x[3] * n.x() + f_y[3] * n.y();
+        // Compute fluxes
+        normal_flux[0] = rho * v_normal;
+        for (int i = 0; i < dim_; i++)
+        {
+            normal_flux[i + 1] = rho * v(i) * v_normal + p * normal(i);
+        }
+        normal_flux[dim_ + 1] = (E + p) * v_normal;
 
-        const real_t sound = std::sqrt(gamma_ * p / rho);
-        const real_t speed = std::sqrt(2 * KE / rho);
+        // Max. wavespeed
+        const double speed = v_mag;
+        const double sound = std::sqrt(gamma_ * p / rho);
 
         return sound + speed;
     }
--- src/discretization/fvm/hyperbolic/euler.hpp
+++ src/discretization/fvm/hyperbolic/euler.hpp
@@ -4,24 +4,19 @@
 
 namespace sfem::fvm
 {
-    /// @brief Flux function for the two-dimensional Euler equations
-    /// @note Subject to change
-    class Euler2D : public FluxFunction
+    class EulerFlux : public FluxFunction
     {
     public:
-        Euler2D(real_t gamma);
+        EulerFlux(real_t gamma, int dim);
 
         real_t compute_flux(const std::vector<real_t> &state, std::vector<real_t> &flux, int dir) const override;
 
         real_t compute_normal_flux(const std::vector<real_t> &state,
-                                   const geo::Vec3 &n,
+                                   const geo::Vec3 &normal,
                                    std::vector<real_t> &normal_flux) const override;
 
     private:
-        std::array<real_t, 4> compute_flux_x(real_t rho, real_t u, real_t v, real_t E, real_t p) const;
-        std::array<real_t, 4> compute_flux_y(real_t rho, real_t u, real_t v, real_t E, real_t p) const;
-
-        /// @brief Specific heat ratio
+        /// @brief Adiabatic index
         real_t gamma_;
     };
 }
