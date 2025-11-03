18e662b57d6b586f588b0dfefc1b05eb4b47e843
Dimitris Pallas <mc19054@mail.ntua.gr>
2025-11-03 23:18:26 +0200

    Minor change

--- src/discretization/fvm/utils/ode_utils.cpp
+++ src/discretization/fvm/utils/ode_utils.cpp
@@ -20,8 +20,8 @@ namespace sfem::fvm::ode
             const auto flux = nflux->flux_function();
 
             // Left and right state fluxes and normal flux
-            std::vector<real_t> state_left(flux->n_comp());
-            std::vector<real_t> state_right(flux->n_comp());
+            std::vector<real_t> state1(flux->n_comp());
+            std::vector<real_t> state2(flux->n_comp());
             std::vector<real_t> normal_flux(flux->n_comp());
 
             // Add flux contribution
@@ -31,13 +31,13 @@ namespace sfem::fvm::ode
                                   int facet_idx)
             {
                 // Get the facet's adjacent cells
-                const auto [cell_idx_left, cell_idx_right] = V->facet_adjacent_cells(facet_idx);
+                const auto [cell_idx1, cell_idx2] = V->facet_adjacent_cells(facet_idx);
 
                 // Get the adjacent cell states
                 for (int i = 0; i < flux->n_comp(); i++)
                 {
-                    state_left[i] = S(cell_idx_left, i);
-                    state_right[i] = S(cell_idx_right, i);
+                    state1[i] = S(cell_idx1, i);
+                    state2[i] = S(cell_idx2, i);
                 }
 
                 // Facet area and normal vector
@@ -45,21 +45,21 @@ namespace sfem::fvm::ode
                 const auto normal = V->facet_normal(facet_idx);
 
                 // Compute the (numerical) normal flux at the face
-                nflux->compute_normal_flux(state_left, state_right, normal, normal_flux);
+                nflux->compute_normal_flux(state1, state2, normal, normal_flux);
 
                 // Inverse left and right cell volumes
-                const real_t vol_inv_left = 1 / V->cell_volume(cell_idx_left);
-                const real_t vol_inv_right = 1 / V->cell_volume(cell_idx_right);
+                const real_t vol_inv1 = 1 / V->cell_volume(cell_idx1);
+                const real_t vol_inv2 = 1 / V->cell_volume(cell_idx2);
 
                 // Add the flux contributions to the RHS vector
                 for (int i = 0; i < flux->n_comp(); i++)
                 {
-                    rhs(cell_idx_left, i) -= normal_flux[i] * area * vol_inv_left;
+                    rhs(cell_idx1, i) -= normal_flux[i] * area * vol_inv1;
 
                     /// @todo cleanup
-                    if (cell_idx_left != cell_idx_right)
+                    if (cell_idx1 != cell_idx2)
                     {
-                        rhs(cell_idx_right, i) -= -normal_flux[i] * area * vol_inv_right;
+                        rhs(cell_idx2, i) -= -normal_flux[i] * area * vol_inv2;
                     }
                 }
             };
