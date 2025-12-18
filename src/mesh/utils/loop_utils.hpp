5ee39de32418e87615326042efa19c0a5a1b5ebc
Dimitris Pallas <mc19054@mail.ntua.gr>
2025-12-18 02:11:35 +0200

    Added for_all_facets_region()

--- src/mesh/utils/loop_utils.hpp
+++ src/mesh/utils/loop_utils.hpp
@@ -40,6 +40,30 @@ namespace sfem::mesh::utils
         }
     }
 
+    /// @brief Loop over the facets of a specific region of a mesh
+    /// @param mesh Mesh
+    /// @param func Function to be executed for every facet
+    /// @param region Region name
+    /// @param skip_ghost Whether to skip ghost facets
+    inline void for_all_facets_region(const Mesh &mesh, MeshLoopFunc auto &&func, const Region &region, bool skip_ghost = true)
+    {
+        // Facet dimension
+        const int facet_dim = mesh.pdim() - 1;
+
+        // Loop over all facets in region
+        for (const auto &[facet, facet_idx] : mesh.region_facets(region.name()))
+        {
+            // Skip ghost facets if required
+            if (skip_ghost and mesh.topology()->entity_index_map(facet_dim)->is_ghost(facet_idx))
+            {
+                continue;
+            }
+
+            // Do work
+            func(mesh, region, facet, facet_idx);
+        }
+    }
+
     /// @brief Loop over the facets of a mesh
     /// @param mesh Mesh
     /// @param func Function to be executed for every facet
@@ -47,9 +71,6 @@ namespace sfem::mesh::utils
     /// @param skip_boundary Whether to skip boundary regions
     inline void for_all_facets(const Mesh &mesh, MeshLoopFunc auto &&func, bool skip_ghost = true, bool skip_boundary = false)
     {
-        // Dimension of facets
-        const int facet_dim = mesh.pdim() - 1;
-
         // Loop over all regions
         for (const auto &region : mesh.regions())
         {
@@ -59,18 +80,19 @@ namespace sfem::mesh::utils
                 continue;
             }
 
-            // Loop over all facets in region
-            for (const auto &[facet, facet_idx] : mesh.region_facets(region.name()))
-            {
-                // Skip ghost facets if required
-                if (skip_ghost and mesh.topology()->entity_index_map(facet_dim)->is_ghost(facet_idx))
-                {
-                    continue;
-                }
+            for_all_facets_region(mesh, func, region, skip_ghost);
+            // // Loop over all facets in region
+            // for (const auto &[facet, facet_idx] : mesh.region_facets(region.name()))
+            // {
+            //     // Skip ghost facets if required
+            //     if (skip_ghost and mesh.topology()->entity_index_map(facet_dim)->is_ghost(facet_idx))
+            //     {
+            //         continue;
+            //     }
 
-                // Do work
-                func(mesh, region, facet, facet_idx);
-            }
+            //     // Do work
+            //     func(mesh, region, facet, facet_idx);
+            // }
         }
     }
 }
